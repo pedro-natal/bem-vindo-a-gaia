5b9a0072b6fe3679f7eb6618bb1734f896b56d3b
pedro-natal <pedropferreiranatal@gmail.com>
2024-01-22 14:54:05 -0300

    criação de nova tela e atualização no JavaScript

---
+++ index.js
@@ -0,0 +1,27 @@
+const lista = [
+  {
+    id: "231589",
+    nome: "Lucas",
+    time: "Amarelo",
+  },
+  {
+    id: "502850",
+    nome: "Pedro",
+    time: "Vermelho",
+  },
+  {
+    id: "296041",
+    nome: "Tiago",
+    time: "Verde",
+  },
+  {
+    id: "612095",
+    nome: "Vitor",
+    time: "Azul",
+  },
+];
+
+function checarId(){
+    var idUsuario = document.getElementById('id').value;
+    if
+}
