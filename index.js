ff926549afbba17d57d91852fc609ce30fb2b7b0
pedro-natal <pedropferreiranatal@gmail.com>
2024-02-07 18:09:28 -0300

    adicionados novos objetos

--- index.js
+++ index.js
@@ -1,23 +1,343 @@
 const lista = [
   {
-    id: "231589",
-    nome: "Lucas",
+    id: "77983047",
+    nome: "Thomas",
     time: "Amarelo",
   },
   {
-    id: "502850",
-    nome: "Pedro",
+    id: "10361059",
+    nome: "Vini",
+    time: "Amarelo",
+  },
+  {
+    id: "48675792",
+    nome: "Gabi Wakaki",
+    time: "Amarelo",
+  },
+  {
+    id: "15612355",
+    nome: "Chico",
+    time: "Amarelo",
+  },
+  {
+    id: "63010762",
+    nome: "Poliana",
+    time: "Amarelo",
+  },
+  {
+    id: "87199586",
+    nome: "Alex",
+    time: "Amarelo",
+  },
+  {
+    id: "57219675",
+    nome: "Matheus Yudi",
+    time: "Amarelo",
+  },
+  {
+    id: "85640961",
+    nome: "Rafa Andrade",
+    time: "Amarelo",
+  },
+  {
+    id: "31319306",
+    nome: "Rafaela Silva",
+    time: "Amarelo",
+  },
+  {
+    id: "31987775",
+    nome: "Laura",
+    time: "Amarelo",
+  },
+  {
+    id: "12788901",
+    nome: "Amarelo1",
+    time: "Amarelo",
+  },
+  {
+    id: "26125427",
+    nome: "Amarelo2",
+    time: "Amarelo",
+  },
+  {
+    id: "26513020",
+    nome: "Amarelo3",
+    time: "Amarelo",
+  },
+  {
+    id: "29367890",
+    nome: "Victor Antunes",
     time: "Vermelho",
   },
   {
-    id: "296041",
-    nome: "Tiago",
+    id: "67282653",
+    nome: "Mateus Alexandre",
+    time: "Vermelho",
+  },
+  {
+    id: "78180857",
+    nome: "Anna Be",
+    time: "Vermelho",
+  },
+  {
+    id: "47359447",
+    nome: "Antônio",
+    time: "Vermelho",
+  },
+  {
+    id: "70420653",
+    nome: "Eloah",
+    time: "Vermelho",
+  },
+  {
+    id: "28240645",
+    nome: "Yuri",
+    time: "Vermelho",
+  },
+  {
+    id: "40848105",
+    nome: "Kaick",
+    time: "Vermelho",
+  },
+  {
+    id: "84153989",
+    nome: "Tiago Marques",
+    time: "Vermelho",
+  },
+  {
+    id: "22947642",
+    nome: "Valentina",
+    time: "Vermelho",
+  },
+  {
+    id: "13234462",
+    nome: "Rebeca",
+    time: "Vermelho",
+  },
+  {
+    id: "33772364",
+    nome: "Vitória",
+    time: "Vermelho",
+  },
+  {
+    id: "61834289",
+    nome: "Vermelho1",
+    time: "Vermelho",
+  },
+  {
+    id: "17257256",
+    nome: "Vermelho2",
+    time: "Vermelho",
+  },
+  {
+    id: "64609521",
+    nome: "Vermelho3",
+    time: "Vermelho",
+  },
+  {
+    id: "26656995",
+    nome: "Samuel",
     time: "Verde",
   },
   {
-    id: "612095",
-    nome: "Vitor",
+    id: "60167054",
+    nome: "André",
+    time: "Verde",
+  },
+  {
+    id: "Henrique",
+    nome: "96649465",
+    time: "Verde",
+  },
+  {
+    id: "20778917",
+    nome: "Iris",
+    time: "Verde",
+  },
+  {
+    id: "72703584",
+    nome: "Vitor Valente",
+    time: "Verde",
+  },
+  {
+    id: "35519120",
+    nome: "Clara",
+    time: "Verde",
+  },
+  {
+    id: "65308481",
+    nome: "Melissa",
+    time: "Verde",
+  },
+  {
+    id: "94629951",
+    nome: "João Pedro",
+    time: "Verde",
+  },
+  {
+    id: "89589567",
+    nome: "Lívia",
+    time: "Verde",
+  },
+  {
+    id: "16190139",
+    nome: "Débora",
+    time: "Verde",
+  },
+  {
+    id: "25450069",
+    nome: "Ana Carolina",
+    time: "Verde",
+  },
+  {
+    id: "46804020",
+    nome: "Verde1",
+    time: "Verde",
+  },
+  {
+    id: "57415961",
+    nome: "Verde2",
+    time: "Verde",
+  },
+  {
+    id: "22623873",
+    nome: "Verde3",
+    time: "Verde",
+  },
+  {
+    id: "11294687",
+    nome: "Tiago Natal",
     time: "Azul",
+  },
+  {
+    id: "68184152",
+    nome: "João Rosa",
+    time: "Azul",
+  },
+  {
+    id: "53499386",
+    nome: "Annelise",
+    time: "Azul",
+  },
+  {
+    id: "75416574",
+    nome: "Keth",
+    time: "Azul",
+  },
+  {
+    id: "92662407",
+    nome: "Thimoteo",
+    time: "Azul",
+  },
+  {
+    id: "14099076",
+    nome: "Mariana Tiago",
+    time: "Azul",
+  },
+  {
+    id: "53283475",
+    nome: "Pedro Grillo",
+    time: "Azul",
+  },
+  {
+    id: "77006991",
+    nome: "Jonatas Tiago",
+    time: "Azul",
+  },
+  {
+    id: "48954900",
+    nome: "Bruna Antunes",
+    time: "Azul",
+  },
+  {
+    id: "79709847",
+    nome: "Manu",
+    time: "Azul",
+  },
+  {
+    id: "78752519",
+    nome: "Azul1",
+    time: "Azul",
+  },
+  {
+    id: "98335566",
+    nome: "Azul2",
+    time: "Azul",
+  },
+  {
+    id: "97138676",
+    nome: "Azul3",
+    time: "Azul",
+  },
+  {
+    id: "98375746",
+    nome: "Lucas Matuoka",
+    time: "Laranja",
+  },
+  {
+    id: "72257665",
+    nome: "Ariel",
+    time: "Laranja",
+  },
+  {
+    id: "83352272",
+    nome: "Zazá",
+    time: "Laranja",
+  },
+  {
+    id: "20093936",
+    nome: "Carol Lemos",
+    time: "Laranja",
+  },
+  {
+    id: "75499286",
+    nome: "João Marcelo",
+    time: "Laranja",
+  },
+  {
+    id: "80949729",
+    nome: "Heloisa",
+    time: "Laranja",
+  },
+  {
+    id: "65380810",
+    nome: "Gabriel",
+    time: "Laranja",
+  },
+  {
+    id: "44084183",
+    nome: "Júlio César",
+    time: "Laranja",
+  },
+  {
+    id: "40738294",
+    nome: "Letícia",
+    time: "Laranja",
+  },
+  {
+    id: "86572182",
+    nome: "Sarah",
+    time: "Laranja",
+  },
+  {
+    id: "66937247",
+    nome: "Esther",
+    time: "Laranja",
+  },
+  {
+    id: "60375807",
+    nome: "Laranja1",
+    time: "Laranja",
+  },
+  {
+    id: "84423230",
+    nome: "Laranja2",
+    time: "Laranja",
+  },
+  {
+    id: "44131513",
+    nome: "Laranja3",
+    time: "Laranja",
   },
 ];
 
